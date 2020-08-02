713c2665755dc946d829cdd50b149a717958f654
groana <contact@groanamelendez.com>
2020-08-02 17:20:21 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -2,10 +2,10 @@
 
 count = 10
 
-while (count >= 1) do
+while (count >= 0) do
   puts count
-  count--
-  # if count === 0
-  #   puts "Happy New Year!"
-  # end
+  count -= 1
+  if count === 0
+    puts "Happy New Year!"
+  end
 end
